f70a18ecbe3f018ae255dc631d3c1d0e27414b3b
sulsans <sulthonazkaradiaskoro@gmail.com>
2024-08-03 17:55:10 +0700

    Create discordbot.py
    bot discord

---
+++ discordbot.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+import random
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hi! I am a bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def choose(ctx, *choices: str):
+    await ctx.send(random.choice(choices))
+  
+
+bot.run("")
